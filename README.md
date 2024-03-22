76a95a1485f779887378ef10e509382c9d4af243
Amine <amine.amzai@etu.minesparis.psl.eu>
2024-03-22 12:02:07 +0100

    Commit du projet

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # ue22-p23-web-calculator
 
-This assignement was given during ue22-p21 web course at Les Mines de Paris in 2022, (https://github.com/ue22-p23/web-intro) Licence CC BY-NC-ND Thierry Parmentelat.
-
-The assignment is to write the **javascript companion** so that
-
-* the device **actually does calculations**
-* in a first step, ignore the '.' and just write an integer calculator
-
-Please commit your code on a personal repo forked from this one.
+Ceci est le projet de Amine AMZAI. 
+Avancer pas à pas était utile pour bien appréhender le problème. 
+Seulement des fois pour augmenter le niveau de fonctionnalité il fallait augmenter le niveau d'abstraction du code 
+(typiquement rajouter un storednumber et un currentnumber, traiter le - quand c'est une opération ou comme indicateur d'un nombre négatif)
+Cela m'a mené à repondre le code de bout en bout pour l'adapter. 
+Les print consoles étaient très utiles pour le débugage. 
+J'ai modifié le css pour que la taille de l'afficheur ne change pas après appuie sur le bouton "AC" (ça faisait un effet un peu bizzare...)
--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,79 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+let display = document.querySelector('.calculator__display');
+let currentNumber = '';
+let currentOperation = ''; 
+let storedNumber = '';
+let lastButtonWasOperation = false;
+let thereisOperation = false;
+
+/*
+Les flags précédent sont utilisés pour gérer les calculs ainsi que pour 
+éviter les bugs.
+*/
+
+window.addEventListener("load",() => {
+    let bouttons = document.querySelectorAll("button")
+    for (let boutton of bouttons) {
+        boutton.addEventListener("click", (evenement) => {
+        const bouton = evenement.target;
+        console.log(bouton.textContent);
+        // Si le bouton est un nombre ou un point
+        if (!isNaN(bouton.textContent) || (bouton.textContent == "-" && currentNumber == '')) {
+            currentNumber += bouton.textContent;
+            display.textContent = currentNumber;
+            lastButtonWasOperation = false;
+        } else if (bouton.textContent == ".") {
+            currentNumber += bouton.textContent;
+            display.textContent = currentNumber;
+        }
+        // Pour clear l'afficheur
+        else if (bouton.textContent == "AC") {
+            currentNumber = '';
+            storedNumber = '';
+            display.textContent = currentNumber;
+            thereisOperation = false;
+            lastButtonWasOperation = false;
+        }
+        // Gérer les opérations
+        else if (["+", "-", "×", "÷"].includes(bouton.textContent)) {
+            if (lastButtonWasOperation && (bouton.textContent == "×" || bouton.textContent == "÷")) {
+                return;
+            }
+            if (thereisOperation) {
+                return;
+            }
+            currentOperation = bouton.textContent;
+            storedNumber = currentNumber;
+            currentNumber = '';
+            display.textContent += currentOperation;
+            lastButtonWasOperation = true;
+            thereisOperation = true;
+        }
+        // Gérer le bouton égal 
+        if (bouton.textContent == "=") {
+            if (currentOperation == '+') {
+                currentNumber = parseFloat(currentNumber) + parseFloat(storedNumber);
+            }
+            else if (currentOperation == '-') {
+                currentNumber = parseFloat(storedNumber) - parseFloat(currentNumber);
+            }
+            else if (currentOperation == '×') {
+                currentNumber = parseFloat(currentNumber) * parseFloat(storedNumber);
+            }
+            else if (currentOperation == '÷') {
+                currentNumber = parseFloat(storedNumber) / parseFloat(currentNumber);
+            }
+            currentOperation = '';
+            lastButtonWasOperation = false;
+            thereisOperation = false;
+            display.textContent = currentNumber;
+        }
+        });
+    }
+})
+
+
+
